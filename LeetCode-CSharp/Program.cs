9dd5a6cb55dcb7ffe68b048fff1b1ab008538596
Harsha kumar G H <harshakumargh@gmail.com>
2025-11-30 17:51:54 -0600

    Bubble Sort

--- LeetCode-CSharp/Program.cs
+++ LeetCode-CSharp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LeetCode_CSharp.Sorting;
+
+BubbleSort bubbleSort = new BubbleSort();
+var result = bubbleSort.Sort(new int[] { 8, 5, 2, 9, 5, 6, 3 });
+for( int i = 0; i < result.Length; i++ )
+{
+    Console.WriteLine(result[i]);
+}
+
+Console.ReadLine();
---
+++ LeetCode-CSharp/Sorting/BubbleSort.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode_CSharp.Sorting
+{
+    public class BubbleSort
+    {
+        public int[] Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++) 
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        var temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
